f36607de104dcbdda8cc04abc0afedc7ed7d8385
cindywindy <salc3657@gmail.com>
2020-12-31 08:53:56 -0700

    Task: Create all possible permutations of a string and return them as a set using recursion. Added Extra: Terminal output is able to show recursive steps.
    Complete

--- stringPermutations.py
+++ stringPermutations.py
@@ -1,9 +1,6 @@
 # Original problem here https://www.interviewcake.com/question/python3/recursive-string-permutations?course=fc1&section=dynamic-programming-recursion
 # Write a recursive function for generating all permutations of an input string. Return them as a set.
 
-#import unittest
-
-
 def get_permutations(string):
     # Edge case
     if len(string) == 0 :
@@ -25,29 +22,3 @@ def get_permutations(string):
 
 # Tests
 print(get_permutations('abcd'))
-
-
-""" class Test(unittest.TestCase):
-
-    def test_empty_string(self):
-        actual = get_permutations('')
-        expected = set([''])
-        self.assertEqual(actual, expected)
-
-    def test_one_character_string(self):
-        actual = get_permutations('a')
-        expected = set(['a'])
-        self.assertEqual(actual, expected)
-
-    def test_two_character_string(self):
-        actual = get_permutations('ab')
-        expected = set(['ab', 'ba'])
-        self.assertEqual(actual, expected)
-
-    def test_three_character_string(self):
-        actual = get_permutations('abc')
-        expected = set(['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
-        self.assertEqual(actual, expected) """
-
-
-#unittest.main(verbosity=2)
