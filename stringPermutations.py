c943a1ff100ef5a017c75ba46e13d6e58c3aac40
cindywindy <salc3657@gmail.com>
2020-12-31 08:48:32 -0700

    Complete!
    Task: Create all possible permutations of a string and return them as a set using recursion.
    Added Extra: Terminal output is able to show recursive steps.

--- stringPermutations.py
+++ stringPermutations.py
@@ -5,18 +5,27 @@
 
 
 def get_permutations(string):
-    permus = set()
-    # handle edge cases
-    if len(string) == 0 or len(string) == 1:
-        return set(string)
-    # base case
-    if len(string) == 2:
+    # Edge case
+    if len(string) == 0 :
+        return set([''])
+    # Base Case
+    if len(string) == 1:
         return set(string)
     # Generate all permutations of the input string
-
-    return set(string)
+    if len(string) >= 2:
+        oldPermut = get_permutations(string[0:len(string)-1])
+        permuts = set()
+        for currentPermut in oldPermut:
+            for strIndx in range(0, len(currentPermut)+1):
+                permuts.add(currentPermut[:strIndx] + string[-1] + currentPermut[strIndx:])
+        print("Permutations for: \"" + currentPermut + string[-1] + "\"")
+        print(permuts)
+        print()
+        return set(permuts)
 
 # Tests
+print(get_permutations('abcd'))
+
 
 """ class Test(unittest.TestCase):
 
